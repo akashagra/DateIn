4c2c48ae06317e9b6d97da9d19671971872c6ec0
Akash Agawal <akashagrawalbest@gmail.com>
2017-09-27 23:19:20 +0530

    Initial Commit

--- app/src/main/java/com/example/avma1997/datein/MainActivity.java
+++ app/src/main/java/com/example/avma1997/datein/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.avma1997.datein;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -14,6 +16,18 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,96 +35,181 @@ import org.json.JSONObject;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    CallbackManager callbackManager;
-    String id,firstName,lastName,gender,fullname;
-
+    CallbackManager mCallbackManager;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseReference;
+    String TAG="fbLogin";
+    FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        callbackManager = CallbackManager.Factory.create();
+        mCallbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton)findViewById(R.id.facebook_login_button);
-        loginButton.setReadPermissions(Arrays.asList("user_location","user_likes","user_photos","user_about_me"));
-
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        loginButton.setReadPermissions("email", "public_profile");
 
 
+        //Create instance of database
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference("user_details");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                setFacebookData(loginResult);
-
-
-
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+                getUserInfo(loginResult);
             }
 
             @Override
             public void onCancel() {
-
+                Log.d(TAG, "facebook:onCancel");
+                // ...
             }
 
             @Override
             public void onError(FacebookException error) {
-
+                Log.d(TAG, "facebook:onError", error);
+                // ...
             }
         });
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //Get currently logged in user
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+
+                    // Name, email address
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getToken() instead.
+                    String uid = user.getUid();
+
+                    //Create user
+                    final User loggedIn = new User(uid, name, email);
+                    Log.i("user",loggedIn.getName());
+
+                    mDatabaseReference.child("users").child(loggedIn.getId()).setValue(loggedIn);
+
+                    mDatabaseReference.child("users").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // This method is called once with the initial value and again
+                            // whenever data at this location is updated.
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // Failed to read value
+                            Log.w(TAG, "Failed to read value.", error.toException());
+                        }
+                    });
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
 
     }
-    private void setFacebookData(LoginResult loginResult) {
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
 
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
 
-        GraphRequest request = GraphRequest.newMeRequest(
-                loginResult.getAccessToken(),
+                        // ...
+                    }
+                });
+    }
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent i = new Intent(this, MatchActivity.class);
+            startActivity(i);
+        }
+    }
+    protected void getUserInfo(final LoginResult login_result){
+
+        GraphRequest data_request = GraphRequest.newMeRequest(
+                login_result.getAccessToken(),
                 new GraphRequest.GraphJSONObjectCallback() {
                     @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-
-
-
-                        // Application code
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
                         try {
-                            Log.i("Response",response.toString());
+                            String facebook_id = object.getString("id");
+                            String f_name = object.getString("name");
+                            String email_id = object.getString("email");
+                            String token = login_result.getAccessToken().getToken();
+                            String picUrl = "https://graph.facebook.com/me/picture?type=normal&method=GET&access_token="+ token;
 
-                            id = response.getJSONObject().getString("id");
-                            firstName = response.getJSONObject().getString("first_name");
-
-                            lastName = response.getJSONObject().getString("last_name");
-                            gender = response.getJSONObject().getString("gender");
-                            Toast.makeText(getApplicationContext(),"Hello "+ firstName+" " + lastName,Toast.LENGTH_SHORT).show();
-                            fullname=  firstName+" "+lastName;
-
-
-
-                            Log.i("Login" + "ID", id);
-                            Log.i("Login"+ "FirstName", firstName);
-                            Log.i("Login" + "LastName", lastName);
-                            Log.i("Login" + "Gender", gender);
-
+                            saveFacebookCredentialsInFirebase(login_result.getAccessToken());
 
                         } catch (JSONException e) {
-                            e.printStackTrace();
+                            // TODO Auto-generated catch block
                         }
                     }
-               });
-
-        Bundle parameters = new Bundle();
-//        parameters.putString("fields", "id,first_name,last_name,gender");
-        request.setParameters(parameters);
-        request.executeAsync();
-
-
-
+                });
+        Bundle permission_param = new Bundle();
+        permission_param.putString("fields", "id,name,email,picture.width(120).height(120)");
+        data_request.setParameters(permission_param);
+        data_request.executeAsync();
+        data_request.executeAsync();
     }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-
-            callbackManager.onActivityResult(requestCode, resultCode, data);
-        Intent i=new Intent(getApplicationContext(),MatchActivity.class);
-        startActivity(i);
-
-
-
-
-
+    private void saveFacebookCredentialsInFirebase(AccessToken accessToken){
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Error logging in", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Login in Successful", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+    @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            // Pass the activity result back to the Facebook SDK
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        }
 }
+
+
---
+++ app/src/main/java/com/example/avma1997/datein/User.java
@@ -0,0 +1,46 @@
+package com.example.avma1997.datein;
+
+/**
+ * Created by Avma1997 on 9/27/2017.
+ */
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+
+    public User(){
+
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
